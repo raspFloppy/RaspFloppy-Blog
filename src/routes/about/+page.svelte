1ec951e07b5f86afd09bff21a3b8d4bfa1e8aa4c
raspFloppy <raspfloppy@protonmail.com>
2023-10-27 09:28:01 +0200

    Updates

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -52,9 +52,10 @@
 		</Heading>
 		<Timeline order="vertical">
 			<TimelineItem title="Movendo Technology" date="March 2022 - Present">
-				<div class="grid grid-cols-8 max-w-xs mt-2 mb-4">
+				<div class="grid grid-cols-9 max-w-xs mt-2 mb-4">
 					<i class="devicon-debian-plain colored" />
 					<i class="devicon devicon-cplusplus-plain colored" />
+					<i class="devicon devicon-python-plain colored" />
 					<i class="devicon devicon-qt-original colored" />
 					<i class="devicon devicon-opencv-plain-wordmark colored" />
 					<i class="devicon-flask-original dark:text-white colored" />
@@ -124,4 +125,63 @@
 	<Hr classHr="w-96 h-1 mx-auto my-4 rounded md:my-10">
 		<QuoteSolid class="w-4 h-4 text-gray-700 dark:text-gray-300" />
 	</Hr>
+
+		<!-- Education Section -->
+		<div id="work-section" class="items-center mt-10">
+			<Heading tag="h2" class="mb-4 md:text-4xl text-3xl underline md:underline-offset-8">
+				Education
+			</Heading>
+			<Timeline order="vertical">
+				<TimelineItem title="University of Genoa" date="September 2021 - Present">
+					<div class="grid grid-cols-8 max-w-xs mt-2 mb-4">
+						<i class="devicon-linux-plain colored " />
+						<i class="devicon devicon-cplusplus-plain colored" />
+						<i class="devicon devicon-c-plain colored" />
+						<i class="devicon devicon-java-plain colored" />
+						<i class="devicon devicon-php-plain colored" />
+						<i class="devicon devicon-git-plain colored" />
+					</div>
+					<P class="text-sm font-normal md:text-base text-gray-600 dark:text-gray-300 mb-2">
+						I am enrolled in the third year of computer science at the University of Genoa, and among the projects I have completed are: 
+						<List>
+							<Li>a</Li>
+							<Li>b</Li>
+							<Li>c</Li>
+						</List>
+					</P>
+				</TimelineItem>
+				<TimelineItem title="Cyberchallange" date="March 2022 - June 2022">
+					<div class="grid grid-cols-8 max-w-xs mt-2 mb-4">
+						<i class="devicon-linux-plain dark:text-white colored" />
+						<i class="devicon-bash-plain dark:text-white colored" />
+						<i class="devicon-javascript-plain colored" />
+						<i class="devicon-git-plain colored" />
+					</div>
+					<P class="text-sm md:text-base font-normal text-gray-600 dark:text-gray-300">
+					</P>
+					<Button href="https://openbadges.bestr.it/public/assertions/ISZ_Ku2KSKWT1-iVRB5lzA" color="alternative" class="mt-2">
+						See my Badge
+						<ArrowUpRightFromSquareOutline class="ml-2 w-3 h-3" />
+					</Button>
+				</TimelineItem>
+				<TimelineItem title="Geco Business Consultant" date="November 2019 - December 2019">
+					<div class="grid grid-cols-8 max-w-xs mt-2 mb-4">
+						<i class="devicon-linux-plain dark:text-white colored" />
+						<i class="devicon-bash-plain dark:text-white colored" />
+						<i class="devicon-docker-plain colored" />
+						<i class="devicon-git-plain colored" />
+					</div>
+					<P class="text-sm md:text-base font-normal text-gray-600 dark:text-gray-300">
+						During high school, I completed a two-month internship at a small consulting company in
+						Genoa. <br />
+						Here, I was able to strengthen my knowledge of Linux and delve into the world of web development
+						and Docker.
+					</P>
+				</TimelineItem>
+			</Timeline>
+		</div>
+	
+		<Hr classHr="w-96 h-1 mx-auto my-4 rounded md:my-10">
+			<QuoteSolid class="w-4 h-4 text-gray-700 dark:text-gray-300" />
+		</Hr>
 </div>
