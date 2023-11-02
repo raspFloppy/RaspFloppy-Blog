2430a8758ed105520a76a8f58fbedd0a2d96caaf
raspFloppy <raspfloppy@protonmail.com>
2023-11-02 07:25:17 +0100

    added github pages compatibility

--- svelte.config.js
+++ svelte.config.js
@@ -32,6 +32,9 @@ const config = {
 	preprocess: [vitePreprocess({}), mdsvex(mdsvexOptions)],
 	kit: {
 		adapter: adapter()
+	},
+	paths: {
+		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 	}
 }
 export default config
